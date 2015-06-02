d280bb7227f2ef41ed7cef0022b4a58190f14a55
Guest User <student@epicodus.com>
2015-06-02 11:10:03 -0700

    allow user to input user name and balance, and will display the information

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function BankAccount(balance, userName, accountNickname) {
+function BankAccount(userName, accountNickname, balance) {
   this.balance = balance;
   this.userName = userName;
   this.accountNickname = accountNickname;
@@ -15,3 +15,18 @@ BankAccount.prototype.deposit = function(amount) {
 BankAccount.prototype.changeNickname = function(newName) {
   return this.accountNickname = newName;
 }
+
+$(document).ready(function() {
+  $("#new-account-form").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedUserName = $("input#input-user-name").val();
+    var inputtedAccountNickname = $("input#input-account-nickname").val();
+    var inputtedBalance = parseInt($("input#input-balance").val());
+
+    var newAccount = new BankAccount(inputtedUserName, inputtedAccountNickname, inputtedBalance)
+
+    $("#account-nickname").text(newAccount.accountNickname)
+    $("#balance").text(newAccount.balance)
+  });
+});
